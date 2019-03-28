e8722a3af03b87c1673a095a618fa777d9b26772
15DCS16 <15DCS16@SVV.local>
2019-03-28 15:38:55 +0530

    amit

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 </div>
 
 <div class="side1">
-    <a href="#" style="background:#27ae60;">Manage Teams</a>
+    <a href="teamdetail.php" style="background:#27ae60;">Manage Teams</a>
     <a href="newmatch.php" style="background:#c0392b;">New Match</a>
     <a href="#" style="background:#2c3e50;">Resume Match</a>
     <a href="#" style="background:#2980b9;">History</a>
---
+++ teamdetail.php
@@ -0,0 +1,25 @@
+<?php include 'header.php'; ?>
+
+
+<div class="wrapper">
+    <form class="form1">
+        <div class="input-fields">
+            <h1>TEAM DETAILS</h1>
+            <input type="text" name="teamname" class="input" placeholder="Team Name" required>
+            <input type="text" name="name1" class="input" placeholder="player Name 1" required>
+            <input type="text" name="name2" class="input" placeholder="player Name 2" required>
+            <input type="text" name="name3" class="input" placeholder="player Name 3" required>
+            <input type="text" name="name4" class="input" placeholder="player Name 4" required>
+            <input type="text" name="name5" class="input" placeholder="player Name 5" required>
+            <input type="text" name="name6" class="input" placeholder="player Name 6" required>
+            <input type="text" name="name7" class="input" placeholder="player Name 7" required>
+            <input type="text" name="name8" class="input" placeholder="player Name 8" required>
+            <input type="text" name="name9" class="input" placeholder="player Name 9" required>
+            <input type="text" name="name10" class="input" placeholder="player Name 10" required>
+            <input type="text" name="name11" class="input" placeholder="player Name 11" required>
+            
+            <input type="submit" name="" value="add">
+        </div>
+    </form>
+</div>
+    <?php include 'footer.php'; ?> 
