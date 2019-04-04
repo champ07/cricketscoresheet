ce0e0ccaa368443595192290ab929bf363bad20b
amitkumar choudhary <amitchowdhary07@gmail.com>
2019-04-04 21:53:31 +0530

    done

--- db_conn.php
+++ db_conn.php
@@ -1,9 +1,9 @@
 <?php
 $dbServername = "localhost";
 $dbUsername = "root";
-$dbPassword = "root";
+$dbPassword = "";
 $dbName = "cricketscoresheet";
-$port = 3308;
+$port = 3306;
 //connection
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName, $port);
  
